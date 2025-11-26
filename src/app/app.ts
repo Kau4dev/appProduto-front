d82551686cb81de0207b2d685519f3ce1f17e89f
kau4dev <kauavictordev7@gmail.com>
2025-11-25 22:27:39 -0300

    feat: adiciona componente ProductCard com template e estilos

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { HeroSection } from './components/hero-section/hero-section';
+import { ProductCard } from './components/product-card/product-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, HeroSection],
+  imports: [RouterOutlet, Navbar, HeroSection, ProductCard],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
