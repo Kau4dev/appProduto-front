94619f1f69d586523e7e841b51f383ea2ea0ba64
kau4dev <kauavictordev7@gmail.com>
2025-11-26 09:43:29 -0300

    feat: adiciona componente Footer com HTML, CSS e testes

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { HeroSection } from './components/hero-section/hero-section';
 import { ProductCard } from './components/product-card/product-card';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, HeroSection, ProductCard],
+  imports: [RouterOutlet, Navbar, HeroSection, ProductCard, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
