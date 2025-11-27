864048c1027634ac7f2d6bc2edd44adcab5255cc
kau4dev <kauavictordev7@gmail.com>
2025-11-27 19:32:00 -0300

    refactor: remove importações não utilizadas de Navbar, HeroSection e ProductCard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from './components/navbar/navbar';
-import { HeroSection } from './components/hero-section/hero-section';
-import { ProductCard } from './components/product-card/product-card';
 import { Footer } from './components/footer/footer';
 import { Home } from './pages/home/home';
 
