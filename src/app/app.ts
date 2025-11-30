c0490dc66899087b5ebff0cfaa58942811e797a1
kau4dev <kauavictordev7@gmail.com>
2025-11-30 14:45:50 -0300

    feat: adiciona componente Navbar e atualiza texto do botão na seção Hero

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,3 +1,4 @@
+import { Navbar } from './components/navbar/navbar';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
@@ -5,7 +6,7 @@ import { Home } from './pages/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Home],
+  imports: [RouterOutlet, Navbar, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
